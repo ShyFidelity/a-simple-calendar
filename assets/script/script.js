e1bfe14518e4dd4d87e609fd10ac883efc5aa025
jkworks <info@jilliankayworks.com>
2021-07-26 13:01:24 -0600

    near finished had issues with first repo so this one is new and doesn't have many commits

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,31 +12,30 @@ var hours = [$('#9'),$('#10'),$('#11'),$('#12'),$('#13'),$('#14'),$('#15'),$('#1
 
 for (let i = 0; i < hours.length; i++) {
     const hoursEl = hours[i].attr("id");
-
-    console.log(hoursEl)
+    hours[i].children('input').val(JSON.parse(localStorage.getItem(hoursEl)) || "")
+   //in for loop take the element take the children of input and set the value to what is to be in ls
 
     if (hoursEl < currentTime) {
-        hours[i].children('#user-input').addClass('past')
+        hours[i].children('input').addClass('past')
         console.log("this is thr past")
     
     }
     else if (hoursEl == currentTime) {
-        hours[i].children('#user-input').addClass('present')
+        hours[i].children('input').addClass('present')
         console.log("this is right now")
     }
 
     else {
-        hours[i].children('#user-input').addClass('future')
+        hours[i].children('input').addClass('future')
         console.log("this is future")
     }
 }
 
 
   
-$(".saveBtn").on("click", function (){
-console.log("I'm saved")
-console.log(saveInput)
-    var userInput = $('input[name="user-input"]').val;
-    localStorage.setItem(userInput.attr('userInput'), JSON.stringify(userInput).val())
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault()
+    var userInput = $(this).siblings("input");
+    localStorage.setItem(userInput.parent().attr('id'), JSON.stringify(userInput.val()))
     
-});
+})
